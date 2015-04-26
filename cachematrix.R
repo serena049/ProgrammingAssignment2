78d45a9c920e0bc03cca29bc5ab8948ab3edec05
serena049 <Wei@Weis-MacBook-Air.local>
2015-04-26 16:36:00 -0400

    final

--- cachematrix.R
+++ cachematrix.R
@@ -13,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
         get<-function() x
         setinverse<-function(inverse) i<<-inverse
         getinverse<-function() i
-        list(set=set,get=get.setinverse = setinverse, getinverse =getinverse)
+        list(set=set,get=get,setinverse = setinverse, getinverse =getinverse)
 
 }
 
